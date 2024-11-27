a14027558e182ccf2a55e251cd89e1263dffb82c
Guilherme Urbano <guilhermeurbano@MacBook-Pro-de-Guilherme.local>
2024-11-27 16:28:36 +0000

    :tada:

---
+++ get_next_line.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: guisanto <guisanto@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/27 15:25:13 by guisanto          #+#    #+#             */
+/*   Updated: 2024/11/27 15:48:16 by guisanto         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+int open (const char* path, int flags [, int mode ])
+{
+    
+}
+ssize_t read(int fildes, void *buf, size_t nbyte)
+{
+    
+}
+
+int main()
+{
+    int fd;
+    fd = open("text.txt", O_RDONLY)
+}
---
+++ get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: guisanto <guisanto@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/27 15:28:44 by guisanto          #+#    #+#             */
+/*   Updated: 2024/11/27 15:45:03 by guisanto         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef     GET_NEXT_LINE_H
+# define    GET_NEXT_LINE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#endif
+
